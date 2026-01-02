165c5eb2cb04df32f5e240c02ca4514e6856d392
Rhys Preston <prestonr@cardiff.ac.uk>
2026-01-02 13:49:31 +0000

    inital class commit

---
+++ src/Homeowner.java
@@ -0,0 +1,3 @@
+public class Homeowner extends User {
+
+}
--- src/User.java
+++ src/User.java
@@ -1,4 +1,4 @@
-public class App {
+public class User {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
